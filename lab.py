309fcec913a494a488603299dde28d0a3cf47048
purcellconsult <purcellconsult@gmail.com>
2020-06-17 13:44:39 -0700

    Update lab.py
    Nice work, just some slight syntax suggestions. Finish the rest of the lab, and ping me on Slack once you do so.

--- lab.py
+++ lab.py
@@ -1,8 +1,11 @@
 
-var1,var2=4,5.75
+var1, var2= 4, 5.75
+
 print("Orignal numbers of var1 and var2 are {}, {}" .format(var1, var2))
 print ("task one to swap the two variables")
-var1,var2=var2,var1
+
+var1, var2 =var2, var1
+
 print("new var1 is {}, new var2 is {}" .format(var1,var2))
 
 if var1 > var2:
@@ -10,19 +13,21 @@ if var1 > var2:
 else:
     print ("The larger number between var1 and var2 is {}" .format(var2))
 
-str1,str2="This is string1","This is string 2"
-if len(str1) > len(str2):
-    print ("the larger string between str1 and str2 is {}" .format(str1))
-else:
-    print ("The larger string between str1 and str2 is '{}'" .format(str2))
+str1, str2= "This is string1","This is string 2"
 
-for x in range(1,101):
-    if x % 2==0:
+if len(str1) > len(str2):
+    print("the larger string between str1 and str2 is {}" .format(str1))
+else:
+    print("The larger string between str1 and str2 is '{}'" .format(str2))
+
+for x in range(1, 101):
+    if x % 2 == 0:
         print(x, end=' ')
-x=1
+ 
 print()
+x=1
 print("while loop practise")
-while x in range(1,101):
+while x in range(1, 101):
     if x % 2 ==0:
         print(x,end=" ")
     x+=1
